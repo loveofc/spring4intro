fd08aa90da244e804886c990654f368239dd1bcf
용환 <lyhlyhlyhlyhlyh@daum.net>
2022-10-04 15:39:51 +0900

    c

--- sp4-chap02/src/main/java/chap02/Main.java
+++ sp4-chap02/src/main/java/chap02/Main.java
@@ -1,7 +1,5 @@
 package chap02;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -13,11 +11,11 @@ public class Main {
 		System.out.println(msg);
 		ctx.close();
 
-		ApplicationContext ac = new AnnotationConfigApplicationContext(ConfigTest.class);
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ConfigTest.class);
 		Greeter2 g2 = ac.getBean("greeter2", Greeter2.class);
 		String msg2 = g2.greet("스프링4");
 		System.out.println(msg2);
-		((ConfigurableApplicationContext) ac).close();
+		ac.close();
 
 	}
 }
