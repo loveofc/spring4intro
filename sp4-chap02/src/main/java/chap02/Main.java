3261f96e53b283aa1581cdbf22c5b589b372f9bc
용환 <lyhlyhlyhlyhlyh@daum.net>
2022-10-04 15:39:13 +0900

    c

--- sp4-chap02/src/main/java/chap02/Main.java
+++ sp4-chap02/src/main/java/chap02/Main.java
@@ -1,7 +1,5 @@
 package chap02;
 
-
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,19 +7,17 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) {
-		GenericXmlApplicationContext ctx = 
-		new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 		Greeter g = ctx.getBean("greeter", Greeter.class);
 		String msg = g.greet("스프링");
 		System.out.println(msg);
-		ctx.close();	
-		
-		/*
-		 * ApplicationContext ac = new
-		 * AnnotationConfigApplicationContext(ConfigTest.class); Greeter2 g2 =
-		 * ac.getBean("greeter2",Greeter2.class); String msg2 = g2.greet("스프링4");
-		 * System.out.println(msg2); ((ConfigurableApplicationContext)ac).close();
-		 */
+		ctx.close();
+
+		ApplicationContext ac = new AnnotationConfigApplicationContext(ConfigTest.class);
+		Greeter2 g2 = ac.getBean("greeter2", Greeter2.class);
+		String msg2 = g2.greet("스프링4");
+		System.out.println(msg2);
+		((ConfigurableApplicationContext) ac).close();
 
 	}
 }
