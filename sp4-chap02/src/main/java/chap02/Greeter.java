c03dbbf3aa3db778a09339e3dea092795b9f1bab
용환 <lyhlyhlyhlyhlyh@daum.net>
2022-10-04 14:37:02 +0900

    c

--- sp4-chap02/src/main/java/chap02/Greeter.java
+++ sp4-chap02/src/main/java/chap02/Greeter.java
@@ -2,12 +2,13 @@ package chap02;
 
 public class Greeter {
 	private String format;
-
+	
 	public String greet(String guest) {
 		return String.format(format, guest);
 	}
-
+	
 	public void setFormat(String format) {
 		this.format = format;
 	}
+
 }
